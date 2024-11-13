bc220b20d5f77176464243a9620e6185c564e4b9
Sasha <alecsand.unit@gmail.com>
2024-11-14 01:23:43 +0200

    dz

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 public class Person
 {
     public string Name { get; set; }
-    public int Age { get; set; }
+    public int Age { get; set; } 
     public string City { get; set; }
 }
 
